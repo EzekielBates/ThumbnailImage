7c7ae8bac2cbcfe848ddd03aba6056e79c2f210a
BATES <eb0888960@otc.edu>
2018-09-25 18:43:59 -0500

    FinalCommit

--- thumbnail.js
+++ thumbnail.js
@@ -15,5 +15,8 @@ images[2] = img3;
 for(let i = 0; i < images.length; ++i){
     document.write("<img src='" + images[i].src + "' width='100' height='100' class='smallImage'/>");
 }
-
-
+for(let i = 0; i < images.length; ++i){
+document.getElementsByTagName("img")[i].onclick = function(){
+    document.getElementById("largeImage").innerHTML = "<img src='" + images[i].src + "' width='300' height='300' class='smallImage'/>";
+    };
+}
