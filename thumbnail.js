c434d02fa716e46b2614aa7e3198915ce2b3f785
BATES <eb0888960@otc.edu>
2018-09-25 18:46:55 -0500

    actual final commit

--- thumbnail.js
+++ thumbnail.js
@@ -1,22 +1,15 @@
 images = new Array();
 
-var img = new Image(100,100);
-var img2 = new Image(100,100);
-var img3 = new Image(100,100);
 
-img.src = "bird.jpg";
-img2.src = "man.jpg";
-img3.src = "cat.jpg";
-
-images[0] = img;
-images[1] = img2;
-images[2] = img3;
+images[0] = "bird.jpg";
+images[1] = "cat.jpg";
+images[2] = "man.jpg";
 
 for(let i = 0; i < images.length; ++i){
-    document.write("<img src='" + images[i].src + "' width='100' height='100' class='smallImage'/>");
+    document.write("<img src='" + images[i] + "' width='100' height='100' class='smallImage'/>");
 }
 for(let i = 0; i < images.length; ++i){
 document.getElementsByTagName("img")[i].onclick = function(){
-    document.getElementById("largeImage").innerHTML = "<img src='" + images[i].src + "' width='300' height='300' class='smallImage'/>";
+    document.getElementById("largeImage").innerHTML = "<img src='" + images[i] + "' width='300' height='300' class='smallImage'/>";
     };
 }
