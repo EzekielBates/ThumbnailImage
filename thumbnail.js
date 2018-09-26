b4ad6ac7f828347dee05b9eb88713fd3ba175188
BATES <eb0888960@otc.edu>
2018-09-26 09:51:18 -0500

    Changed tomake the imags randomly change with a timer

--- thumbnail.js
+++ thumbnail.js
@@ -8,8 +8,19 @@ images[2] = "man.jpg";
 for(let i = 0; i < images.length; ++i){
     document.write("<img src='" + images[i] + "' width='100' height='100' class='smallImage'/>");
 }
+
 for(let i = 0; i < images.length; ++i){
 document.getElementsByTagName("img")[i].onclick = function(){
     document.getElementById("largeImage").innerHTML = "<img src='" + images[i] + "' width='300' height='300' class='smallImage'/>";
     };
+}
+
+document.getElementById("largeImage").innerHTML = "<img src='" + images[0] + "' width='300' height='300' class='smallImage'/>";
+
+myTimer = setInterval(randomImage,3000);
+
+function randomImage(){
+    var myRand;
+    myRand = Math.floor(Math.random() * (images.length));
+    document.getElementById("largeImage").innerHTML = "<img src='" + images[myRand] + "' width='300' height='300' class='smallImage'/>";
 }
